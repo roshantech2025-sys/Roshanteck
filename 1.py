d62fb484dfd9802ebaea7073a7e9d1ba4ee6d085
roshantech2025-sys <roshantech2025@gmail.com>
2025-11-18 16:56:37 +0330

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+import tkinter
+window=tkinter.Tk()
+window.title("اطلاعات")
+window.configure(bg="#F5EFE7",height="300px",width="300px")
+window.geometry("350x350+500+300")
+#label1=Label(window,text="نام",bg="#3E5879")
+#label1.place(x=200,y=10)
+button=Button(window,text="dhgg")
+button.pack()
+window.mainloop
